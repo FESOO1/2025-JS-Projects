4539b839fb7c713b86b51f32d326eace008270d3
FES001 <firdavs7794948@gmail.com>
2025-05-12 00:26:33 +0500

    Working on it.

---
+++ snake/script.js
@@ -0,0 +1,24 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+// MAP
+const map = [
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+];
+
+// DRAW
+
+function draw() {
+    
+
+    requestAnimationFrame(draw);
+};
+
+draw();
